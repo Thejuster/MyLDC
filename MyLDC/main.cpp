fdab514da83a35bd250aecba1e88ae3125942744
Thejuster <realquaker@hotmail.it>
2016-10-27 23:43:17 +0200

    First binary serialization test

--- MyLDC/main.cpp
+++ MyLDC/main.cpp
@@ -1,9 +1,29 @@
 #include "main.h"
 
 extern "C" __declspec(dllexport) void __stdcall InfoSistema();
+extern "C" __declspec(dllexport) std::string __stdcall Serializza(std::string valore);
 
 void Info::InfoSistema()
 {
 
     MessageBoxA(0,"Ciao Daemond", "DLL Message", MB_OK | MB_ICONINFORMATION);
+}
+
+
+std::string Info::Serializza(std::string valore)
+{
+
+    char* test = new char[20];
+	memset(test,0x0,20);
+	int i = 5;
+	memcpy (test, &i, sizeof(int));
+	std::ostringstream sout (std::ios_base::binary);
+	sout.write(test, 20);
+        int ipos = sout.tellp();
+
+	// testout points to the start of some garbage....
+	const char* testout = sout.str().data();
+
+
+	return sout.str();
 }
--- MyLDC/main.h
+++ MyLDC/main.h
@@ -1,11 +1,12 @@
 
 #include <windows.h>
 #include <stdio.h>
+#include <sstream>
 
 class Info
 {
 
 public:
    void InfoSistema();
-
+   std::string Serializza(std::string valore);
 };
